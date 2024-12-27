6cbb1538c6279c4e65c528246493db26403ce190
kevin <ahadjon1999bb@gmail.com>
2024-12-27 12:39:34 +0900

    feat: meta tags intergraions on document private file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,24 @@ export default function Document() {
 
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index, follow" />
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+
+        {/* SEO */}
+        <meta
+          name="keyboard"
+          content={"nestar, nestar.uz, devex mern, mern nestjs fullstack"}
+        />
+        <meta
+          name="description"
+          content={
+            "Buy and sell properties anywhere and anytime in Seoul Korea | "
+          }
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
